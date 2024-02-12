022da5c2d0531ed65a0695669f3d3fb65a2bd19f
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2024-02-12 12:25:37 +0300

    local build

--- Formula/protoc-gen-connect-swift.rb
+++ Formula/protoc-gen-connect-swift.rb
@@ -1,14 +1,18 @@
 class ProtocGenConnectSwift < Formula
   desc "Swift support for Connect protocol"
   homepage "https://github.com/connectrpc/connect-swift"
-  url "https://github.com/connectrpc/connect-swift/releases/download/0.12.0/protoc-gen-connect-swift.tar.gz"
-  sha256 "545f14582eb57c0f8724f710c80c0636354dc01f7ab7fe9275bb1e5d9cbcd816"
+  url "https://github.com/connectrpc/connect-swift/archive/refs/tags/0.12.0.tar.gz"
+  sha256 "3ddc6062b5eea9ea83bb82a957387874c7d4624cabff68b92ec0b6791b7adfda"
   license "Apache-2.0"
 
   depends_on xcode: ["15.2", :build]
+  depends_on "protobuf"
+
+  uses_from_macos "swift"
 
   def install
-    bin.install "protoc-gen-connect-swift" => "protoc-gen-connect-swift"
+    system "make", "buildplugins"
+    bin.install ".tmp/bin/protoc-gen-connect-swift" => "protoc-gen-connect-swift"
   end
 
   test do
